e6f43e66cc1f6fc110b375ff1808c58e8b86cfaa
Maickeenn <gabriel_amaro_sp@hotmail.com.br>
2021-03-27 21:31:11 -0300

    ajuste CORS

--- src/main/java/br/com/smiles/Autenticacao/controller/AutenticacaoController.java
+++ src/main/java/br/com/smiles/Autenticacao/controller/AutenticacaoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/login")
 public class AutenticacaoController {
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = "*")
     @GetMapping("/{idCliente}")
     private ResponseEntity<Sessao> getCliente(@PathVariable String idCliente,
                                               @RequestParam(value = "usuario") String usuario,
@@ -19,14 +19,14 @@ public class AutenticacaoController {
         return ResponseEntity.ok(Sessao.builder().idCliente(idCliente).build());
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = "*")
     @GetMapping("/sessao")
     private ResponseEntity<Sessao> getSessao(@RequestParam(value = "sessao") Sessao sessao){
 
         return ResponseEntity.ok(sessao);
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = "*")
     @PostMapping("/{idCliente}")
     private ResponseEntity<Login> updateCliente(@RequestBody Sessao idCliente){
 
